9c9e306b2a85ad4ef2c71541ac2192c878899b51
Matheus Luciano <matheus2909.meg45@gmail.com>
2025-10-08 18:12:54 -0300

    Add files via upload

---
+++ LobotomyEnterprise/LobotomyEnterprise/Empresa.java
@@ -0,0 +1,19 @@
+public class Empresa{
+    private SetorDeDesenvolvimento setor_desenvolvimento;
+
+
+    public Empresa(SetorDeDesenvolvimento setor_desenvolvimento){
+        this.setor_desenvolvimento = setor_desenvolvimento;
+    }
+
+
+
+    public SetorDeDesenvolvimento getSetor_desenvolvimento() {
+        return this.setor_desenvolvimento;
+    }
+
+    public void setSetor_desenvolvimento(SetorDeDesenvolvimento setor_desenvolvimento) {
+        this.setor_desenvolvimento = setor_desenvolvimento;
+    }
+   
+}
