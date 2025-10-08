2a6f8d8debd06a3fe42a559b9efd9362f97762d1
Renan Aprigio Dias de Moura <renan.dias3158@gmail.com>
2025-10-08 16:58:35 -0300

    2° commit lobotomy_enterprise
    continuação da construção das classes Java
               -Renan

--- LobotomyEnterprise/Setor.java
+++ LobotomyEnterprise/Setor.java
@@ -15,10 +15,27 @@ public class Setor {
                     this.projetos_Secundarios = projetos_Secundarios;
                     this.funcionarios = funcionarios;
                     this.estagiarios = estagiarios;
-                    this.
+                    this.id_Setor = id_Setor;
                  }
+         
 
-    
 
+   public String GetNome(){return nome;}
+   public void SetNome(String nome){this.nome = nome;}
+
+   public Proj GetProjeto_Principal(){return projeto_Principal;}
+   public void SetProjeto_Principal(String projeto_Principal){this.projeto_Principal = projeto_Principal;}
+   
+   public List<Proj> GetProjetos_Secundario(){return projetos_Secundarios;}
+   public void SetProjetos_Secundarios(List<Proj>){this.projetos_Secundarios = projetos_Secundarios;}
+   
+   public List<Funcionarios> GetFuncionarios(){return funcionarios;}
+   public void SetFuncionarios(List<Funcionarios> funcionarios){this.funcionarios = funcionarios;}
+
+   public List<Estagiarios> GetEstagiarios(){return estagiarios;}
+   public void SetEstagiarios(List<Estagiarios> estagiarios){this.estagiarios = estagiarios;}
+
+   public String GetId_Setor(){return id_Setor;}
+   public void SetId_Setor(String id_Setor){this.id_Setor = id_Setor;}
 
 }
