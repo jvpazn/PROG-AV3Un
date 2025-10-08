a1a87f8fca442713bdbfe7995f2b50fe90272270
Renan Aprigio Dias de Moura <renan.dias3158@gmail.com>
2025-10-08 06:08:21 -0300

    Update Lobotomy_enterprise
    1° commit da construção das classes do projeto.
           - de Renan

---
+++ LobotomyEnterprise/Empresa.java
@@ -0,0 +1,11 @@
+public class Empresa{
+    private setor_desenvolvimento SetorDeDesenvolvimento;
+
+    public Empresa(setor_desenvolvimento SetorDeDesenvolvimento){
+        this.SetorDeDesenvolvimento = SetorDeDesenvolvimento;
+    }
+
+
+    public setor_desenvolvimento GetSetorDeDesenvolvimento(){return SetorDeDesenvolvimento;}
+    public void SetSetorDeDesenvolvimento(setor_desenvolvimento SetorDeDesenvolvimento){this.SetorDeDesenvolvimento = SetorDeDesenvolvimento;}
+}
---
+++ LobotomyEnterprise/Setor.java
@@ -0,0 +1,24 @@
+import java.util.List;
+
+public class Setor {
+    private String nome;
+    private Proj projeto_Principal;
+    private List<Proj> projetos_Secundarios;
+    private List<Funcionarios> funcionarios;
+    private List<Estagiarios> estagiarios;
+    private String id_Setor;
+
+    public Setor(String nome, Proj projeto_Principal, List<Proj> projetos_Secundarios,
+                 List<Funcionarios> funcionarios, List<Estagiarios> estagiarios, String id_Setor){
+                    this.nome = nome;
+                    this.projeto_Principal = projeto_Principal;
+                    this.projetos_Secundarios = projetos_Secundarios;
+                    this.funcionarios = funcionarios;
+                    this.estagiarios = estagiarios;
+                    this.
+                 }
+
+    
+
+
+}
